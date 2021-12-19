d45b7e00fb206bbdf1f5f44ae92d03b6da0dfcfe
IbrahimRassool <202100769@learner.open.ac.mu>
2021-12-19 18:38:12 +0400

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,4 +1,4 @@
-const topCard = document.getElementsByClassName("top_card")[0]
+const topCard = document.getElementsByClassName("top_card")[0];
 const currencyHolder = document.getElementById("currency");
 const currentBalanceHolder = document.getElementById("balance");
 const tnxNameHolder = document.getElementById("name");
@@ -6,95 +6,110 @@ const tnxAmountHolder = document.getElementById("amount");
 const income = document.getElementById("income");
 const expense = document.getElementById("expense");
 const saveButton = document.getElementById("save");
-const displayList = document.getElementById("list_of_transactions");
-const cancelEditButton = document.getElementById("cancel_edit");
+const displayList = document.getElementById("list_of_transaction");
 
-let symbol="R";
-let listOfTransaction=[];
-let currentBalance= 0;
+let symbol = "R";
 
+var listOfTransaction = JSON.parse(window.localStorage.getItem("list")) || [];
 
-function edit(i){
-editIndex=i;
-tnxNameHolder.value=listOfTransaction[i].name;
-tnxAmountHolder.value=listOfTransaction[i].amount;
-if(listOfTransaction[i].type=="income"){income.checked= true
-}
-else{
-  expense.checked=true
-}
+let currentBalance = 0;
+
+function del(i) {
+  listOfTransaction = listOfTransaction.filter((e, index) => i !== index);
+  render();
 }
 
-function del(i){
-listOfTransaction=listOfTransaction.filter((e, index) => i !== index);
-render()
-
-}
-
-function saveData(){
+function saveData() {
   localStorage.setItem("symbol", symbol);
   localStorage.setItem("balance", currentBalance);
   localStorage.setItem("list", JSON.stringify(listOfTransaction));
 }
 
-function loadData(){
-  symbol =localStorage.setItem("symbol");;
-  currentBalance=Number(JsolocalStorage.setItem("balance"));;
-  listOfTransaction=JSON.parse(localStorage.setItem("list"));;
+function loadData() {
+  symbol = localStorage.setItem("symbol");
+  currentBalance = Number(JsolocalStorage.setItem("balance"));
+  listOfTransaction = JSON.parse(localStorage.setItem("list"));
 }
 
-function render(){
-  currentBalance = listOfTransaction.reduce(function(total,value){
-  if (amount.type==income)return (total +value)
-  else return(total-value), 0})
+function render(firstLoad = true) {
+  let currentBalance = 0;
 
+  console.log(listOfTransaction);
+  let currentExpenses = listOfTransaction.reduce(function (total, value) {
+    return value.type == "expense" ? total + value.amount : total + 0;
+  }, 0);
 
- displayList.innerHTML="";
+  let currentIncome = listOfTransaction.reduce(function (total, value) {
+    return value.type == "income" ? total + value.amount : total + 0;
+  }, 0);
 
- if(listOfTransaction.length=0){
-   displayList.innerHTML+="No Transactions found!"
- }
+  currentBalance = listOfTransaction.reduce(function (total, value) {
+    return value.type == "income" ? total + value.amount : total - value.amount;
+  }, 0);
 
- else{
-   listOfTransaction.forEach((e,i)=>{
-     displayList.innerHTML+=
-     <li class="transaction R{e.type}">
-      <p>R{e.name}</p>
-     <div class="right_side">
-       <p>R{symbol}R{e.amount}</p>
-       < button onclick="edit(R{i})"><i class="fa-solid fa-pen-to-square"></i></button>
-       <button onclick="del(R{i})"><i class="fa-thin fa-trash-can"></i></button>
-     </div>
-   </li>
-   })
- }
+  console.log("List of expenses is ", currentExpenses);
 
+  displayList.innerHTML = "";
 
-  currencyHolder.innerHTML="symbol"
-  balanceHolder.innerHTML="currentBalance"
-  saveData();
+  if (listOfTransaction.length == 0) {
+    displayList.innerHTML += "No Transactions found!";
+  } else {
+    listOfTransaction.map((e, i) => {
+      displayList.innerHTML += `
+    <li class='transaction ${e.type == "income" ? "income" : "expense"}' >
+      <p>${e.name}</p>
+    <div class="right_side">
+      <p>${symbol + " " + e.amount}</p>
+      <button onclick="del(${i})"><i class="fa-thin fa-trash-can"></i>Delete</button>
+    </div>
+  </li>`;
+    });
+  }
+
+  currencyHolder.innerHTML = symbol;
+  currentBalanceHolder.innerHTML = currentBalance;
+  if (!firstLoad) {
+    saveData();
+  }
+
+  new Chart("transactionsPieChart", {
+    type: "pie",
+    data: {
+      labels: ["Income", "Expense"],
+      datasets: [
+        {
+          label: "Expense Breakdown",
+          data: [currentIncome, currentExpenses],
+          backgroundColor: ["rgb(255,99,132)", "rgb(54,162,235)"],
+          hoverOffset: 4,
+        },
+      ],
+    },
+  });
 }
 
-saveButton.addEventListener("click", () => {
-if (tnxNameHolder.value=="" || tnxAmountHolder.value <= 0){
-  alert("can't do that!");
-  return;
-}
-  let transaction= {
+saveButton.addEventListener("click", function () {
+  if (tnxNameHolder.value == "" || tnxAmountHolder.value <= 0) {
+    alert("can't do that!");
+    return;
+  }
+  let transaction = {
     name: tnxNameHolder.value,
-    amount: Number(tnxNameHolder.value),
-    type: income.checked? "income" : "expense"
+    amount: Number(tnxAmountHolder.value),
+    type: income.checked ? "income" : "expense",
   };
 
-  if(editIndex== -1)
+  if (editIndex == -1) {
+    console.log("Editing");
     listOfTransaction.push(transaction);
-  else
-   listOfTransaction(editIndex)= transaction;
+  } else {
+    console.log("Not Editing");
+    listOfTransaction[editIndex] = transaction;
+  }
+  editIndex = -1;
+  tnxNameHolder.value = "";
+  tnxAmountHolder.value = "";
 
-   editIndex=-1;
-    tnxNameHolder.value="";
-    tnxAmountHolder.value="";
-    render();
-})
+  render((firstLoad = false));
+});
 
-render()
